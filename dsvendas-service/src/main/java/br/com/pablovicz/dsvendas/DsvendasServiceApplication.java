62bc34cce0928883b0b1a57601fd3618be550802
Pablo Woinarovicz Ramos <pablowoina2205@gmail.com>
2021-09-08 09:03:50 -0300

    Project Created

---
+++ dsvendas-service/src/main/java/br/com/pablovicz/dsvendas/DsvendasServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.pablovicz.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasServiceApplication.class, args);
+	}
+
+}
---
+++ dsvendas-service/src/test/java/br/com/pablovicz/dsvendas/DsvendasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pablovicz.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

6b894d4d177c66e15041e56a4d3294b47b67f719
Pablo Woinarovicz Ramos <pablowoina2205@gmail.com>
2021-09-11 09:24:04 -0300

    Pagination

---
+++ dsvendas-service/src/main/java/br/com/pablovicz/dsvendas/services/SaleService.java
@@ -0,0 +1,32 @@
+package br.com.pablovicz.dsvendas.services;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.pablovicz.dsvendas.dto.SaleDTO;
+import br.com.pablovicz.dsvendas.entities.Sale;
+import br.com.pablovicz.dsvendas.repositories.SaleRepository;
+import br.com.pablovicz.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(s -> new SaleDTO(s));
+	}
+
+}

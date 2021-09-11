adb28356c17d1768684c632b084d1eb5ff9ca260
Pablo Woinarovicz Ramos <pablowoina2205@gmail.com>
2021-09-11 14:49:30 -0300

    BarChart integration

--- dsvendas-webapp/src/types/sale.ts
+++ dsvendas-webapp/src/types/sale.ts
@@ -1,4 +1,10 @@
 export type SalesSum = {
-    "sellerName": string,
-    "sum": number
-}
+  sellerName: string;
+  sum: number;
+};
+
+export type SaleSucess = {
+  sellerName: string;
+  visited: number;
+  deals: number;
+};
